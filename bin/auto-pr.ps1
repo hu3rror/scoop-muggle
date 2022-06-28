78e62d006d58ecb6ccaee4836a9be876908261a6
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2022-06-28 12:13:09 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "hu3rror/scoop-muggle:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
