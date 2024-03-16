782989702c1065e9fd1913304bae62a14724bf8a
hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-03-16 14:21:00 +0800

    refactor: add missing ISSUE_TEMPLATE files
    This commit adds the missing ISSUE_TEMPLATE files for bug-report, config, hash-error, package-request, and issue_comment.
    
    Additionally, the CI workflow for tests has been updated to run pwsh and powershell jobs on main and master branches only.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "hu3rror/scoop-muggle:master"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
